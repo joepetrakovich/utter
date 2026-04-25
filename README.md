5eeda263b520533334139b91b11c7c54b8a858b6
Joe Petrakovich <jpetrakovich@gmail.com>
2026-04-25 20:16:01 +0900

    Added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+utter:
+
+A minimal and offline speech-to-text utility for MacOS.
+
+Press a hotkey, speak, automatically paste the transcription into the active app.
--- Utter/UtterApp.swift
+++ Utter/UtterApp.swift
@@ -7,26 +7,86 @@
 
 import SwiftUI
 import SwiftData
+import AppKit
+
+
+func handleKeyDown(
+    proxy: CGEventTapProxy,
+    type: CGEventType,
+    cgEvent: CGEvent,
+    userInfo: UnsafeMutableRawPointer?
+) -> Unmanaged<CGEvent>? {
+    
+    if let nsEvent = NSEvent(cgEvent: cgEvent),
+       nsEvent.type == .keyDown {
+        // check if CMD pressed
+        //let cmdPressed = nsEvent.modifierFlags.contains(.command)
+        // get current key press
+        let pressedChar = nsEvent.charactersIgnoringModifiers?.lowercased() ?? ""
+        // was CMD + H pressed?
+        if pressedChar == "h" {
+            print("H pressed")
+            return nil // swallow event
+        }
+    }
+    // Let all other keystrokes pass
+    return Unmanaged.passUnretained(cgEvent)
+}
 
 @main
 struct UtterApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
+        
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }()
-
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra {
+            //ContentView()
+            Button("Action 1") { /* Logic */ }
+            Button("Action 2") { /* Logic */ }
+            Divider()
+            Button("Quit") { NSApplication.shared.terminate(nil) }
+        } label: {
+            Label("Utter", systemImage: "waveform")
         }
         .modelContainer(sharedModelContainer)
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var eventTap: CFMachPort?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Initial setup code here
+        print("here")
+        // define a keydown event
+        let eventMask = (1 << CGEventType.keyDown.rawValue)
+        if let tap = CGEvent.tapCreate(
+            tap: .cgSessionEventTap,
+            place: .headInsertEventTap,
+            options: .defaultTap,
+            eventsOfInterest: CGEventMask(eventMask),
+            callback: handleKeyDown,
+            userInfo: nil
+        ) {
+            self.eventTap = tap
+            let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0)
+            CFRunLoopAddSource(CFRunLoopGetMain(), runLoopSource, .commonModes)
+            CGEvent.tapEnable(tap: tap, enable: true)
+        } else {
+            print("Failed to create event tap.")
+        }
     }
 }
